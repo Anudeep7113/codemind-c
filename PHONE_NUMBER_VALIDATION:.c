029a3f1e890101185dfcadb609a6d795e4a65f32
Anudeep7113 <97575253+Anudeep7113@users.noreply.github.com>
2022-06-08 22:00:08 +0530

    #include<stdio.h> int main() {     int n,r,c=0;     scanf("%d",&n);     while(n>0)     {         r=n%10;         c++;         n=n/10;     }     if (c<9||r==0||c>10)     {         printf("Invalid");     }     else     {         printf("Valid");     } }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,c=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        c++;
+        n=n/10;
+    }
+    if (c<9||r==0||c>10)
+    {
+        printf("Invalid");
+    }
+    else
+    {
+        printf("Valid");
+    }
+}
