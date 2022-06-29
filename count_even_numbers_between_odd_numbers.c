e752ce40c1f800dcdeccd23e21d3b8b1160429ed
Anudeep7113 <97575253+Anudeep7113@users.noreply.github.com>
2022-06-29 21:50:47 +0530

    #include<stdio.h> int main() {     int i,c=0,n,a[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=1;i<(n-1);i++)     {         if(a[i-1]%2==1 && a[i+1]%2==1)         {             if(a[i]%2==0)             {             c++;             }         }     }     printf("%d",c); }

---
+++ count_even_numbers_between_odd_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,c=0,n,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<(n-1);i++)
+    {
+        if(a[i-1]%2==1 && a[i+1]%2==1)
+        {
+            if(a[i]%2==0)
+            {
+            c++;
+            }
+        }
+    }
+    printf("%d",c);
+}
