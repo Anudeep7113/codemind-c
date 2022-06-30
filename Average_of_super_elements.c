1acdf63953ff6d52a5480ed36b3ba33425341427
Anudeep7113 <97575253+Anudeep7113@users.noreply.github.com>
2022-06-30 20:30:26 +0530

    #include<stdio.h> int main() {     int n,i,j,a[100],c=0,m=0,b[100],k=0,sum=0;     float avg,d;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(a[i]==a[j])             {                 c++;             }         }         if(c==a[i])         {             m++;             b[k]=a[i];             sum+=b[k];             a[i]=0;             k++;         }     }   d=sum;   if(m==0)   printf("-1");   else   {    avg=d/float(m);    printf("%.2f",avg);   } }

--- Average_of_super_elements.c
+++ Average_of_super_elements.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int a[100],i,j,n,c,b[100],k=0,sum=0;
+    int n,i,j,a[100],c=0,m=0,b[100],k=0,sum=0;
     float avg,d;
     scanf("%d",&n);
     for(i=0;i<n;i++)
@@ -20,21 +20,19 @@ int main()
         }
         if(c==a[i])
         {
+            m++;
             b[k]=a[i];
             sum+=b[k];
             a[i]=0;
             k++;
         }
     }
-    d=sum;
-    if(k==0)
-    {
-    printf("-1");
-    }
-    else
-    {
-        avg=float(d)/k;
-         printf("%.2f",avg);
-    }
-   
+  d=sum;
+  if(m==0)
+  printf("-1");
+  else
+  {
+   avg=d/float(m);
+   printf("%.2f",avg);
+  }
 }
