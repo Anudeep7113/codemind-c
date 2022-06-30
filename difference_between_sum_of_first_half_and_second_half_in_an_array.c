85d0dc32574bbb39be33f9a988b750c25e6570f9
Anudeep7113 <97575253+Anudeep7113@users.noreply.github.com>
2022-06-30 22:08:07 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,a[100],d,sum=0,sum1=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n/2;i++)     {         sum+=a[i];     }     for(i=n/2;i<n;i++)     {         sum1+=a[i];     }     d=sum-sum1;     printf("%d",abs(d)); }

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,a[100],d,sum=0,sum1=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n/2;i++)
+    {
+        sum+=a[i];
+    }
+    for(i=n/2;i<n;i++)
+    {
+        sum1+=a[i];
+    }
+    d=sum-sum1;
+    printf("%d",abs(d));
+}
