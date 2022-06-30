cfc892fc81e474c83d1d35abb338edba98e56ea5
Anudeep7113 <97575253+Anudeep7113@users.noreply.github.com>
2022-06-30 22:28:40 +0530

    #include<stdio.h> int main() {     int i,n,c=0,a[i];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n-1;i++)     {         if(a[i]>a[i+1])         {             c++;         }     }     if(c==n-1)     printf("yes");     else     printf("no"); }

---
+++ Descending_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,c=0,a[i];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        if(a[i]>a[i+1])
+        {
+            c++;
+        }
+    }
+    if(c==n-1)
+    printf("yes");
+    else
+    printf("no");
+}
