b9fc7ba70ee739ce0b3de0100ec538d74bc23c73
Anudeep7113 <97575253+Anudeep7113@users.noreply.github.com>
2022-06-30 21:58:30 +0530

    #include<stdio.h> int main() {     int m,n,i,a[100],r,k=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d%d",&m,&r);     for(i=0;i<n;i++)     {         if(a[i]>=m && a[i]<=r)         {             printf("%d ",a[i]);             k=1;         }     }     if(k==0)     printf("-1"); }

---
+++ elements_in_between_A_and_B.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,i,a[100],r,k=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d%d",&m,&r);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=m && a[i]<=r)
+        {
+            printf("%d ",a[i]);
+            k=1;
+        }
+    }
+    if(k==0)
+    printf("-1");
+}
