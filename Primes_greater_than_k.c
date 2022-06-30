a61aa53c65109a14be90d4ddb1947f164ee2ef55
Anudeep7113 <97575253+Anudeep7113@users.noreply.github.com>
2022-06-30 22:13:29 +0530

    #include<stdio.h> int main() {     int n,i,j,c1,c2,k,a[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         for(j=1;j<=a[i];j++)         {             if(a[i]%j==0)             {                 c1++;             }         }         if(c1==2)         {             if(a[i]>=k)             {                 c2++;             }         }         c1=0;     }     printf("%d",c2);         return 0; }

---
+++ Primes_greater_than_k.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,c1,c2,k,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        for(j=1;j<=a[i];j++)
+        {
+            if(a[i]%j==0)
+            {
+                c1++;
+            }
+        }
+        if(c1==2)
+        {
+            if(a[i]>=k)
+            {
+                c2++;
+            }
+        }
+        c1=0;
+    }
+    printf("%d",c2);
+        return 0;
+}
