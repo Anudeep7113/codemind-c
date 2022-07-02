2452f633dba08ebc174083a4efb73735918019db
Anudeep7113 <97575253+Anudeep7113@users.noreply.github.com>
2022-07-02 20:12:55 +0530

    #include<stdio.h> #include<string.h> int main() {     int i,c=0,len,min;     char s[100];     scanf("%[^ ]s",s);     len=strlen(s);     for(i=0;s[i]!=NULL;i++)     {         if(s[i]==' ' || i==len-1)         {             min=c;             c=0;             break;         }         else         {             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')             {                 c++;             }         }     }     for(i=0;s[i]!=NULL;i++)     {         if(s[i]==' ' || i==len-1)         {             if(min>c)             {                 min=c;             }             c=0;         }         else         {            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')            {                c++;            }         }     }     printf("%d",min); }

---
+++ minimum_vowel_count_in_a_word.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,c=0,len,min;
+    char s[100];
+    scanf("%[^
+]s",s);
+    len=strlen(s);
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]==' ' || i==len-1)
+        {
+            min=c;
+            c=0;
+            break;
+        }
+        else
+        {
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+            {
+                c++;
+            }
+        }
+    }
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]==' ' || i==len-1)
+        {
+            if(min>c)
+            {
+                min=c;
+            }
+            c=0;
+        }
+        else
+        {
+           if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+           {
+               c++;
+           }
+        }
+    }
+    printf("%d",min);
+}
