6af8d1f310d0dcc7fe555ca11a8292a79c03fd1b
Anudeep7113 <97575253+Anudeep7113@users.noreply.github.com>
2022-07-02 19:46:10 +0530

    #include<stdio.h> #include<string.h> int main() {     int i,j=0,c;     char s[100],s1[100];     scanf("%[^ ]s",s);     c=strlen(s);     for(i=c-1;i>=0;i--)     {         s1[j]=s[i];         j++;     }     s1[j]=NULL;     printf("%s",s1); }

---
+++ reverse_words_and_positions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,j=0,c;
+    char s[100],s1[100];
+    scanf("%[^
+]s",s);
+    c=strlen(s);
+    for(i=c-1;i>=0;i--)
+    {
+        s1[j]=s[i];
+        j++;
+    }
+    s1[j]=NULL;
+    printf("%s",s1);
+}
