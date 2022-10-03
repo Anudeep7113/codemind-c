4f3311e145f515b871a2dbc63a332843b8b848fd
Anudeep7113 <97575253+Anudeep7113@users.noreply.github.com>
2022-10-03 10:02:07 +0530

    #include<stdio.h> int main() {     int n,i,count=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             count++;         }     }     if(count==2)     {         printf("prime");     }     else     {         printf("not a prime");     }     return 0; }

--- prime_number.c
+++ prime_number.c
@@ -1,28 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-  int n, i, flag = 0;
-  scanf("%d", &n);
-
-  for (i = 2; i <= n / 2; ++i) 
-  {
-    if (n % i == 0)
+    int n,i,count=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
     {
-      flag = 1;
-      break;
+        if(n%i==0)
+        {
+            count++;
+        }
     }
-  }
-
-  
-  if (n == 0 || n == 1) 
-  {
-    printf("%d is neither prime nor composite.", n);
-  } 
-  else 
-  {
-    if (flag == 0)
-      printf("prime");
+    if(count==2)
+    {
+        printf("prime");
+    }
     else
-      printf("not a prime");
-  }
+    {
+        printf("not a prime");
+    }
+    return 0;
 }
